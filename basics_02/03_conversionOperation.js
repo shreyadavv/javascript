6735deb3bb05fbcb807a5f8bf85242943b3100c2
Shreya <128418139+shreyadavv@users.noreply.github.com>
2023-10-02 12:58:31 +0000

    conversion done

--- basics_02/03_conversionOperation.js
+++ basics_02/03_conversionOperation.js
@@ -22,5 +22,43 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNum = 33
 let stringNumber = String(someNum)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ********************OPERATIONS***********************************
+let value=3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-1);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " shreya"
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true); 
+// console.log(+true);//gives 1
+
+// console.log(+"");// gives 0
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100
+gameCounter++;//prefix,postfix
+console.log(gameCounter);
+
+//link to study 
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
