64498bc7f08cdc1c17e1071099b997faaeae0570
Shreya <128418139+shreyadavv@users.noreply.github.com>
2023-10-02 20:40:47 +0000

    comparision

---
+++ basics_02/comparision.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+//console.log("2">1);//datatype should be same while comparision
+//console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);// results are unpredictable
+
+
+// console.log(undefined==0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+//working of other comparision and == is different
+
+//=== strict equality checks both data and datatype
+//avoid comparision between different datatype
